da2163bbc7a52c4f8d17e35852dba8df0d648b38
justinliu6165 <justin_liu@hotmail.co.uk>
2020-07-14 21:57:01 +0100

    Fix: name default database

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         uri: env('DATABASE_URI'),
+        database: "obd-db"
       },
       options: {
         ssl: true,
