af0a3b7940cb3e4efbb7f4212c26d00d70bb9cca
justinliu6165 <justin_liu@hotmail.co.uk>
2020-07-20 22:26:07 +0100

    Process: set up development and production

--- config/database.js
+++ config/database.js
@@ -1,15 +1,39 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        uri: env('DATABASE_URI'),
-        database: "obd-db"
+module.exports = ({ env }) => {
+  if(env('NODE_ENV') === 'production') {
+    return ({
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'mongoose',
+          settings: {
+            uri: env('DATABASE_URI'),
+            database: "obd-db"
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
-      options: {
-        ssl: true,
+    });
+  } else {
+    return ({
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'mongoose',
+          settings: {
+            client: "mongo",
+            host: "localhost",
+            port: "27017",
+            database: "obd-local-db",
+            username: "",
+            password: ""
+          },
+          options: {
+            ssl: false,
+          },
+        },
       },
-    },
-  },
-});
+    });
+  }
+}
